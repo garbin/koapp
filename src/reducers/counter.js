00a5582d2db5078e74990499da02489e0d55ed82
Garbin Huang <garbinh@gmail.com>
2016-03-24 06:02:41 +0000

    . typo

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,12 +1,12 @@
 import {INCREASE, DECREASE} from '../constants';
-import {client} from '../lib/fetch';
+import {api} from '../lib/fetch';
 export default function update(state = 0, action) {
   if (action.type == INCREASE) {
     return state + action.amount;
   } else if (action.type == DECREASE) {
     return state - action.amount;
   } else if(action.type == 'FETCH'){
-    console.log(client.read('/apps'))
+    console.log(api.read('/apps'))
     return state;
   } else {
     return state;
