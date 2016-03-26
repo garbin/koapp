8a357b3a33fc96a5d11003a17491a45369747bb4
Garbin Huang <garbinh@gmail.com>
2016-03-26 04:30:57 +0000

    . typo

--- src/containers/index/index.jsx
+++ src/containers/index/index.jsx
@@ -4,6 +4,11 @@ import {connect} from '../../lib/helper';
 export default connect(state=>({index:state.index}))(class extends React.Component {
   render(){
     var {index} = this.props;
-    return <div>Redux Rocks!{index}</div>
+    return (<div>
+              Redux Rocks!{index}
+              <br />
+              <a href="/counter">Counter</a>&nbsp;|&nbsp;
+              <a href="/signup">Signup</a>
+            </div>)
   }
 })
