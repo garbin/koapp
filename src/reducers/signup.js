f1f5997e77610dad3183e79d4397e179d386ba81
Garbin Huang <garbinh@gmail.com>
2016-03-26 06:42:31 +0000

    . typo

--- src/reducers/signup.js
+++ src/reducers/signup.js
@@ -2,7 +2,7 @@ import {SUBMIT, SUBMIT_DONE, SUBMIT_ERROR} from '../constants'
 import {api} from '../lib/fetch'
 export function signup(state = {username:'', password:''}, action) {
   if (action.type == SUBMIT_DONE) {
-    console.log('Done', action);
+    alert(JSON.stringify(action.payload));
     state = action.payload;
   }
   return state;
