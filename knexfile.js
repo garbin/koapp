48be3ab5a049af4f4b5a5a30a20c38bb81dcbc38
Garbin Huang <garbinh@gmail.com>
2016-11-12 13:41:18 +0800

    * add babel-polyfill to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 // Update with your config settings.
 require('babel-register');
+require('babel-polyfill');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var config = require('./config');
 var _ = require('lodash');
