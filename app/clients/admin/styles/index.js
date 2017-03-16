886d5b5ca6904aacaaf97e967e8b9b3c3ccd783c
Garbin <garbinh@gmail.com>
2017-03-16 17:32:47 +0800

    improve

--- app/clients/admin/styles/index.js
+++ app/clients/admin/styles/index.js
@@ -1,10 +1,10 @@
-import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
-import 'react-select/dist/react-select.css'
 import './js/vendor.min.js'
 import './css/vendor.css'
 import './css/app.css'
 import './css/app-seagreen.css'
 import './css/admin.css'
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
+import 'react-select/dist/react-select.css'
 
 export default {
   primaryColor: '#4bcf99'
