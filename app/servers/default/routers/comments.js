93d6161acc07de5d35b766bd0031a9045f553c9f
Garbin <garbinh@gmail.com>
2017-04-16 21:49:42 +0800

    upgrade koapi -> latest

--- app/servers/default/routers/comments.js
+++ app/servers/default/routers/comments.js
@@ -2,7 +2,7 @@ const { router } = require('koapi')
 const { Comment } = require('../../../models')
 
 exports.default = router.define('resource', {
-  collection: ctx => ctx.state.parents.post.comments(),
+  collection: ctx => ctx.state.nested.post.comments(),
   model: Comment,
   setup (router) {
     router.crud()
