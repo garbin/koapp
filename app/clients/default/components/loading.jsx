a1ad4da11d648748f43c0df2cce83e5c9cdb2000
Garbin <garbinh@gmail.com>
2017-04-20 16:10:23 +0800

    improve

--- app/clients/default/components/loading.jsx
+++ app/clients/default/components/loading.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Loading from 'react-loading'
-import style from '../../styles'
+import style from '../styles'
 
 export default ({loading, children}) => (
   loading ? <Loading delay={0} type='cylon' color={style.primaryColor} /> : children
