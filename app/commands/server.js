c79b9d91785b93dc9ce961daac99284e9cdbe073
Garbin <garbinh@gmail.com>
2017-04-21 17:48:46 +0800

    improve

--- app/commands/server.js
+++ app/commands/server.js
@@ -1,11 +1,15 @@
 exports.default = {
   command: 'server [name]',
   describe: 'run web server',
-  builder: { name: { default: 'default' } },
+  builder: {
+    name: { default: 'default' },
+    config: { alias: 'c' }
+  },
   handler (argv) {
     const { logger: log, config } = require('koapi')
+    const configNS = argv.config || argv.name.split('/')[0]
     log.info(`Using environment: ${process.env.NODE_ENV}`)
     const server = require(`../servers/${argv.name}`)
-    server.start(config(`servers/${argv.name}`).get('port'))
+    server.start(config(`servers/${configNS}`).get('port'))
   }
 }
