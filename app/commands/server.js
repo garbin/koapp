46e5dc4cb371ce29a369afab1f8c7744f034bdcc
Garbin <garbinh@gmail.com>
2017-04-21 17:37:42 +0800

    improve

--- app/commands/server.js
+++ app/commands/server.js
@@ -6,6 +6,6 @@ exports.default = {
     const { logger: log, config } = require('koapi')
     log.info(`Using environment: ${process.env.NODE_ENV}`)
     const server = require(`../servers/${argv.name}`)
-    server.start(config('servers/default').get('port'))
+    server.start(config(`servers/${argv.name}`).get('port'))
   }
 }
