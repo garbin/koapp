76068d3b8327b34d7cbe3026474d684816d41a06
Garbin <garbinh@gmail.com>
2017-05-06 09:30:55 +0800

    improve

--- app/servers/api/routers/aggregate.js
+++ app/servers/api/routers/aggregate.js
@@ -1,14 +1,13 @@
 const { router } = require('koapi')
-const Raw = require('knex/lib/raw')
 const { default: user } = require('../middlewares/user')
-const { User } = require('../../../models')
+const { connection, User } = require('../../../models')
 
 exports.default = router.define('aggregate', router => {
   // /aggregate/users
   router.use(user.grant('admin.aggregate'))
-  router.aggregate(User.collection(), {
+  router.aggregate(User, {
     dimensions: [
-      {name: 'created_date', column: new Raw().set('created_at::date as created_date')}
+      {name: 'created_date', column: connection.raw('created_at::date as created_date')}
     ],
     metrics: [
       {name: 'total', aggregate: 'count', column: 'id as total'}
