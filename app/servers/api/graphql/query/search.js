19d0cffa5d9eeeee399960ba123e942a4e46ffe1
Garbin <garbinh@gmail.com>
2017-05-27 00:37:53 +0800

    improve

--- app/servers/api/graphql/query/search.js
+++ app/servers/api/graphql/query/search.js
@@ -5,11 +5,14 @@ const { Post } = require('../types')
 const SearchType = new types.Enum({
   name: 'SearchType',
   values: {
-    POST: { value: models.Post }
+    POST: {
+      value: ({first, after}) => models.Post.forge().fetchPage({limit: first, offset: after})
+    }
   }
 })
-const Searchable = new types.Union({
-  name: 'Searchable',
+
+const SearchableItem = new types.Union({
+  name: 'SearchableItem',
   types: [ Post ],
   resolveType: model => {
     if (model instanceof models.Post) {
@@ -19,12 +22,12 @@ const Searchable = new types.Union({
 })
 
 module.exports = {
-  type: types.connection.define(Searchable),
+  type: types.connection.define(SearchableItem),
   args: types.connection.args({
     type: types.nonNull(SearchType)()
   }),
-  resolve: types.connection.resolve(async (root, {first, after, type}, ctx) => {
-    const result = await type.forge().fetchPage({ limit: first, offset: after })
+  resolve: types.connection.resolve(async (root, {type, first, after}, ctx) => {
+    const result = await type({first, after})
     return {
       totalCount: result.pagination.rowCount,
       edges: result.models.map((node, index) => ({node, cursor: after + index})),
