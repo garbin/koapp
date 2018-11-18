780c2eee39b27a3e7ad0fea93119946937b25343
Garbin Huang <garbinh@gmail.com>
2018-11-18 19:13:55 +0800

    update

--- app/clients/next/pages/index.js
+++ app/clients/next/pages/index.js
@@ -6,6 +6,7 @@ import provider from '../redux'
 import Link from 'next/link'
 import page from '../components/page'
 import Header from '../components/header'
+import { Container, Row, Col, Card, CardBody, CardLink } from 'reactstrap'
 
 const query = gql`
   query {
@@ -20,6 +21,32 @@ export class Index extends React.Component {
     return (
       <div>
         <Header />
+        <Container className='mt-2'>
+          <Row>
+            <Col xs={3}>
+              <Card>
+                <CardBody>
+                abc
+                  <CardLink href='#'>abc</CardLink>
+                </CardBody>
+              </Card>
+              <Card className='mt-4'>
+                <CardBody>
+                abc
+                  <CardLink href='#'>abc</CardLink>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs={9}>
+              <Card>
+                <CardBody>
+                abc
+                  <CardLink href='#'>abc</CardLink>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }
